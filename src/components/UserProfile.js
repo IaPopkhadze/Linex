c13eecec10c9e2d2e81508c5cd6e139f77dabf67
ia popkhadze <iafofkhadze@gmail.com>
2023-04-27 15:23:15 -0700

    last changes

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineLogout } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
-
+import { BsCheckLg } from "react-icons/bs";
+import { BsCheck2Square } from "react-icons/bs";
 const UserProfile = () => {
   const navigate = useNavigate();
   const [userBalance, setUserBalacer] = useState(100.6);
   const [showOverlay, setShowOverlay] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [checkall, setCheckAll] = useState(false);
   const [percels, setPercels] = useState([
     {
       id: "TKDS$434523",
@@ -14,13 +16,35 @@ const UserProfile = () => {
       location: " თბილისის ოფისი",
       price: 23,
       status: "გადასახდელია",
+      checked: false,
+      request: false,
     },
     {
-      id: "TKDS$434524",
+      id: "TKDS$4345238",
+      weight: "3 კგ",
+      location: " თბილისის ოფისი",
+      price: 23,
+      status: "გადასახდელია",
+      checked: false,
+      request: false,
+    },
+    {
+      id: "TKDS$4345237",
+      weight: "3 კგ",
+      location: " თბილისის ოფისი",
+      price: 23,
+      status: "გადასახდელია",
+      checked: false,
+      request: false,
+    },
+    {
+      id: "TKDS$4341524",
       weight: "46 კგ",
       location: " ზესტაფონის ოფისი",
       price: 343,
       status: "გადასახდელია",
+      checked: false,
+      request: false,
     },
     {
       id: "TKDS$434525",
@@ -28,6 +52,8 @@ const UserProfile = () => {
       location: " ბათუმის ოფისი",
       price: 34,
       status: "გადასახდელია",
+      checked: false,
+      request: false,
     },
     {
       id: "TKDS$434526",
@@ -35,6 +61,8 @@ const UserProfile = () => {
       location: " რუსთავის ოფისი",
       price: 57,
       status: "გადასახდელია",
+      checked: false,
+      request: false,
     },
   ]);
 
@@ -73,6 +101,38 @@ const UserProfile = () => {
   };
   const [currentItem, setCurrentItem] = useState(null);
 
+  const handleCheck = (id) => {
+    const checkedItem = percels.map((element) =>
+      element.id === id ? { ...element, checked: !element.checked } : element
+    );
+    setPercels(checkedItem);
+  };
+
+  const handleRecieve = () => {
+    const recivedItems = percels.map((element) =>
+      element.checked ? { ...element, request: true } : element
+    );
+    setPercels(recivedItems);
+  };
+
+  const handleCheckAll = () => {
+    const checkAllItem = percels.map((element) =>
+      checkall && element.status === "გადახდილია"
+        ? { ...element, checked: false }
+        : { ...element, checked: true }
+    );
+    setPercels(checkAllItem);
+  };
+
+  useEffect(() => {
+    const selected = percels.every((element) => element.checked);
+    if (selected) {
+      setCheckAll(true);
+    }
+  }, [percels]);
+
+  console.log(percels);
+
   return (
     <div className="user_profile">
       <div className="user_profile_header">
@@ -90,91 +150,97 @@ const UserProfile = () => {
         </button>
       </div>
 
-      {/* პირველი */}
-      <div className="table1">
-        <div className="table1_header grid4">
-          <div className="table1_child">ამანათები</div>
-          <div className="table1_child">წონა</div>
-          <div className="table1_child ">მდებარეობა</div>
-          <div className="table1_child">სტატუსი</div>
-        </div>
-        <div className="table_1_info">
-          {percels
-            .filter((x) => x.status === "გადახდილია")
-            .map((element, i) => {
-              return (
-                <div
-                  key={element.id}
-                  className="each_info_container grid4"
-                  style={{
-                    backgroundColor: i % 2 ? "var(--color4)" : "var(--color5)",
+      {percels.some((item) => item.status === "გადახდილია") ? (
+        <div className="table1_container">
+          <div className="table1">
+            <div className="table1_header grid4">
+              <div className="table1_child">
+                <input
+                  onClick={() => {
+                    setCheckAll(!checkall);
+                    handleCheckAll();
                   }}
-                >
-                  <div className="info_child">{element.id}</div>
-                  <div className="info_child text_center">{element.weight}</div>
-                  <div className="info_child text_center">{element.location}</div>
-                  <div
-                    className="info_child text_center"
-                    style={{ color: "#00aa55", fontWeight: "bold" }}
-                  >
-                    {element.status}
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-      </div>
+                  type="checkbox"
+                  style={{ cursor: "pointer", marginRight: "0.4rem" }}
+                />
+              </div>
+              <div className="table1_child">მისაღები ამანათები</div>
+              <div className="table1_child">წონა</div>
+              <div className="table1_child ">მდებარეობა</div>
+              <div className="table1_child">სტატუსი</div>
+            </div>
+            <div className="table_1_info">
+              {percels
 
-      {/* <div className="table">
-        <div className="table_column">
-          <div className="header_title">ამანათები</div>
-          {percels
-            .filter((x) => x.status !== "გადახდილია")
-            .map((element) => {
-              return <div className="info_child">{element.id}</div>;
-            })}
+                .filter((x) => x.status === "გადახდილია")
+                .map((element, i) => {
+                  return (
+                    <div
+                      key={element.id}
+                      className="each_info_container grid4"
+                      style={
+                        element.request
+                          ? { backgroundColor: "#92bd82" }
+                          : {
+                              backgroundColor:
+                                i % 2 ? "var(--color4)" : "var(--color5)",
+                            }
+                      }
+                    >
+                      <div
+                        className="info_child"
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          gap: "0.5rem",
+                        }}
+                      >
+                        {element.request ? (
+                          <BsCheckLg
+                            style={{
+                              color: "green",
+                              fontWeight: "bold",
+                              fontSize: "1.1rem",
+                              marginLeft: "0.5rem",
+                            }}
+                          />
+                        ) : (
+                          <input
+                            checked={element.checked}
+                            onClick={() => handleCheck(element.id)}
+                            type="checkbox"
+                            style={{ marginLeft: "0.5rem", cursor: "pointer" }}
+                          />
+                        )}
+                      </div>
+                      <div className="info_child text_center">{element.id}</div>
+                      <div className="info_child text_center">
+                        {element.weight}
+                      </div>
+                      <div className="info_child text_center">
+                        {element.location}
+                      </div>
+                      <div
+                        className="info_child text_center"
+                        style={{ color: "#00aa55", fontWeight: "bold" }}
+                      >
+                        {element.request
+                          ? "მოთხოვნა მიღებულია"
+                          : element.status}
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+          <div className="recieve_btn_container">
+            <button className="recieve_btn" onClick={handleRecieve}>
+              მიღება
+            </button>
+          </div>
         </div>
-
-        (index===1 ?element.id)
-        <div className="table_column">
-          <div className="header_title">წონა</div>
-          {percels
-            .filter((x) => x.status !== "გადახდილია")
-            .map((element) => {
-              return <div className="info_child">{element.weight}</div>;
-            })}
-        </div>
-        <div className="table_column">
-          <div className="header_title">მდებარეობა</div>
-          {percels
-            .filter((x) => x.status !== "გადახდილია")
-            .map((element) => {
-              return <div className="info_child">{element.location}</div>;
-            })}
-        </div>
-        <div className="table_column">
-          <div className="header_title">სტატუსი</div>
-          {percels
-            .filter((x) => x.status !== "გადახდილია")
-            .map((element) => {
-              return <div className="info_child">{element.status} {element.price} ლ</div>;
-            })}
-        </div>
-        <div className="table_column">
-          <div className="header_title">მოქმედება</div>
-          {percels
-            .filter((x) => x.status !== "გადახდილია")
-            .map((element) => {
-              return (
-                <div className="info_child">
-                  <button className="info_btn" onClick={() => handlePay(element.id, element.price)}>
-                    გადახდა
-                  </button>
-                </div>
-              );
-            })}
-        </div>
-      </div> */}
+      ) : null}
 
       <div className="table1">
         <div className="table1_header">
